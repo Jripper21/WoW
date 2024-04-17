58056401b001c0265012d999afff6507e17d113b
BreannaT07 <144352346+BreannaT07@users.noreply.github.com>
2024-04-17 09:17:38 -0400

    Update enemy.py

--- WoW-Starter-File-main/enemy.py
+++ WoW-Starter-File-main/enemy.py
@@ -1,12 +1,13 @@
+#Using the code from the character file to make the enemy
 from character import Character
 
 class Enemy(Character):
     def __init__(self, name, health, durability, attack_damage):
-        super().__init__(name, health, durability)
+        super().__init__(name, health, durability) #Makes the code more organized and not as big
         self.__attack_damage = attack_damage
         self.__damage_num = 4
-
-
+        
+#The enemy's attack strategy
     def att_strategy(self):
         if self.__damage_num > 8:
             return "uses a killer attack!"
@@ -20,7 +21,8 @@ class Enemy(Character):
     def __str__(self):
         info = super().__str__()
         print(f"{info}\nAttack Damage: {self.__attack_damage}\nDamage: {self.__damage_num}")
-
+        
+#In use since there is a name for the enemy
 if __name__ == "__main__":
     generic_enemy = Enemy("Skeleton", 17, 5, 6)
     print(generic_enemy.attack())
